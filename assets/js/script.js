d859dd48cad4836818b921c41a6fac7f28b18d6d
WojtekKamilowski <kamil.wojciechowski105@gmail.com>
2022-11-21 21:54:06 +0000

    Set up structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,32 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAsnwer() {
+
+}
+
+function displayAdditionQuestion() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+
+}
 
